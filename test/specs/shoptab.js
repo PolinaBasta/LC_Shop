b6bdd0b6360b006eb7b3d66144e4f3d55dc21842
igorLIL <igorlieskov@gmail.com>
2020-12-14 17:18:02 -0800

    IL 1

--- test/specs/shoptab.js
+++ test/specs/shoptab.js
@@ -6,72 +6,58 @@ describe('My Shop tab', () => {
     it('should login and validate Shop ', () => {
         LoginPage.open();
         LoginPage.login('3333@gmail.com','33333');
-        browser.pause(2000);
+        browser.pause(1000);
         expect(MainPage.shopOne.getText()).toEqual(LoginPage.Text);
     });
 
-    it('1', () => {
-        LoginPage.logout();
-        expect(MainPage.shopOne.isDisplayed()).toEqual(false);
-    });
+    // it('1', () => {
+    //     LoginPage.logout();
+    //     expect(MainPage.shopOne.isDisplayed()).eq(false);
+    // });
 
     it('2 ', () => {
         LoginPage.open();
         LoginPage.login('2222@gmail.com', '22222');
-        browser.pause(2000);
+        browser.pause(1000);
         MainPage.Courses.click();
         expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
     });
 
-    // it('3 ', () => {
-    //     browser.url('https://stage.localcoding.us/profile/5fbac7546ec0d6006a8e1d3c');
-    //     browser.pause(2000);
-    //     MainPage.Cards.click();
-    //     expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
-    // });
-    //
-    // it('4', () => {
-    //     browser.url('https://stage.localcoding.us/profile/5fbac7546ec0d6006a8e1d3c');
-    //     browser.pause(2000);
-    //     MainPage.Diary.click();
-    //     expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
-    // });
-    //
-    // it('5', () => {
-    //     browser.url('https://stage.localcoding.us/profile/5fbac7546ec0d6006a8e1d3c')
-    //     // LoginPage.open();
-    //     // LoginPage.login('2222@gmail.com', '22222');
-    //     browser.pause(2000);
-    //     MainPage.Groups.click();
-    //     expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
-    // });
-    //
-    // it('6 ', () => {
-    //     browser.url('https://stage.localcoding.us/profile/5fbac7546ec0d6006a8e1d3c')
-    //     // LoginPage.open();
-    //     // LoginPage.login('2222@gmail.com', '22222');
-    //     browser.pause(2000);
-    //     MainPage.Challenges.click();
-    //     expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
-    // });
-    //
-    // it('7', () => {
-    //     browser.url('https://stage.localcoding.us/profile/5fbac7546ec0d6006a8e1d3c')
-    //     // LoginPage.open();
-    //     // LoginPage.login('2222@gmail.com', '22222');
-    //     browser.pause(2000);
-    //     MainPage.Chat.click();
-    //     expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
-    // });
-    //
-    // it('8', () => {
-    //     browser.url('https://stage.localcoding.us/profile/5fbac7546ec0d6006a8e1d3c')
-    //     // LoginPage.open();
-    //     // LoginPage.login('2222@gmail.com', '22222');
-    //     browser.pause(2000);
-    //     MainPage.Goal.click();
-    //     expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
-    // });
+    it('3 ', () => {
+        browser.pause(1000);
+        MainPage.Cards.click();
+        expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
+    });
+
+    it('4', () => {
+        browser.pause(2000);
+        MainPage.Diary.click();
+        expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
+    });
+
+    it('5', () => {
+        browser.pause(1000);
+        MainPage.Groups.click();
+        expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
+    });
+
+    it('6 ', () => {
+        browser.pause(2000);
+        MainPage.Challenges.click();
+        expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
+    });
+
+    it('7', () => {
+        browser.pause(2000);
+        MainPage.Chat.click();
+        expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
+    });
+
+    it('8', () => {
+        browser.pause(2000);
+        MainPage.Goal.click();
+        expect(MainPage.Shop.getText()).toEqual(LoginPage.Text);
+    });
 
 
 });
